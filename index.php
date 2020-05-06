e499ef4288f15ddb872ee3aa8fae535176966a6e
silviach12 <60413455+silviach12@users.noreply.github.com>
2020-05-06 17:48:30 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include_once 'filesLogic.php';?>
+<html>
+<head>
+<title> Term Scheduling </title>
+</head>
+<body>
+	<p> Term scheduling </p>
+	<form method='POST' enctype= 'multipart/form-data'>
+	<p> Courses:<input type='file' name='courses' />
+	<p> Rooms:<input type='file' name='rooms' />
+	<p> Upload: <input type='submit' name='submit' value='Upload'/></p> 
+	<p> Click here to see the output of scheduled classes <input type='submit' name='submit'/></p>
+	</form>
+</body>
+</html>
+	
